48d70e5b774a5be959a7184db8f9338b0789c324
GannaKov <104867435+GannaKov@users.noreply.github.com>
2024-01-01 18:29:10 +0000

    Deploying to gh-pages from @ GannaKov/ua-military-helper@9aeb490e5e423b7fead3b1329e7b285ab6fd430c 🚀

---
+++ form.053798f8.js
@@ -0,0 +1,2 @@
+new URLSearchParams(window.location.search).get("form"),console.log("in oneFormRender");
+//# sourceMappingURL=form.053798f8.js.map
---
+++ form.1bb8e6f0.js
@@ -0,0 +1,2 @@
+!function(){"use strict";new URLSearchParams(window.location.search).get("form"),console.log("in oneFormRender")}();
+//# sourceMappingURL=form.1bb8e6f0.js.map
